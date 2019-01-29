e045bf423e6488cb2c0068ef29306a6f78ca5d38
f1crazed <f1crazed@gmail.com>
2019-01-29 15:43:02 -0800

    modified:   pfmtodo.py

--- pfmtodo.py
+++ pfmtodo.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,6 +8,14 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return render_template('index.html')
+
+@app.route("/create", methods = ['GET', 'POST'])
+def createtodo():
+    if request.method == 'GET':
+        title = "Create"
+    if request.method == 'POST':
+        ""
+    return render_template('createtodo.html', title=title)
 
 if __name__ == '__main__':
     app.run()
