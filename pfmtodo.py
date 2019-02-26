5950cd245ac769fc816987550dcccc1a86aa489b
Jeremy Drew <f1crazed@gmail.com>
2019-02-26 12:41:01 -0800

    modified:   pfmtodo.py 	modified:   templates/createtodo.html

--- pfmtodo.py
+++ pfmtodo.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 from flask import request, redirect
 from wtforms import Form, TextField, DateField, validators
+
 
 class CreateTodoForm(Form):
     task = TextField("Task: ", validators=[validators.required()])
     due_date = DateField("Due Date:", validators=[validators.required()])
 
 app = Flask(__name__)
+app.secret_key = 'development key'
+
 
 @app.route("/")
 @app.route("/index")
@@ -19,10 +22,11 @@ def createtodo():
     form = CreateTodoForm(request.form)
     if request.method == 'GET':
         title = "Create"
-        return render_template('createtodo.html', title=title)
+        return render_template('createtodo.html', title=title, form=form)
     if request.method == 'POST':
-        print form.task
-        return redirect("index", code=302)
+        if form.validate():
+            print form.task.data
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
