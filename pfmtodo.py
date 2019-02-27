b4befc5b67ffc5b150cb778e02dddb3ddeaae92e
f1crazed <f1crazed@gmail.com>
2019-02-26 16:54:28 -0800

    insert into mongo

--- pfmtodo.py
+++ pfmtodo.py
@@ -2,7 +2,15 @@ from flask import Flask, url_for
 from flask import render_template
 from flask import request, redirect
 from wtforms import Form, TextField, DateField, validators
+from pymongo import MongoClient
+from datetime import datetime
 
+class Todo():
+    def __init__(self):
+        self.task_text = ""
+        self.due_date = None
+        self.create_date = None
+        self.complete_date = None
 
 class CreateTodoForm(Form):
     task = TextField("Task: ", validators=[validators.required()])
@@ -25,6 +33,13 @@ def createtodo():
         return render_template('createtodo.html', title=title, form=form)
     if request.method == 'POST':
         if form.validate():
+            t = Todo()
+            t.task_text = form.task.data
+            t.due_date = form.due_date.data.strftime('%m-%d-%Y')
+            t.create_date = datetime.now().strftime('%m-%d-%Y')
+            client = MongoClient()
+            db = client.pfmTodo
+            db.todos.insert(t.__dict__)
             print form.task.data
             print form.due_date.data
         return redirect(url_for('index'))
