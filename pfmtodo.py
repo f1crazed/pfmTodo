fda292eb05db203fcfc1e956100201f03cec9e83
f1crazed <f1crazed@gmail.com>
2019-02-28 18:04:06 -0800

    Added list rendering and new rout for r completed todos

--- pfmtodo.py
+++ pfmtodo.py
@@ -7,10 +7,17 @@ from datetime import datetime
 
 class Todo():
     def __init__(self):
+        self._id = ""
         self.task_text = ""
         self.due_date = None
         self.create_date = None
         self.complete_date = None
+    def Set(self, obj):
+        self._id = obj['_id']
+        self.task_text = obj['task_text']
+        self.due_date = obj['due_date']
+        self.create_date = obj['create_date']
+        self.complete_date = obj['complete_date']
 
 class CreateTodoForm(Form):
     task = TextField("Task: ", validators=[validators.required()])
@@ -23,7 +30,17 @@ app.secret_key = 'development key'
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    client = MongoClient()
+    db = client.pfmTodo
+    return_todos = db.todos.find()
+    todos = []
+    if return_todos.count() > 0:
+        for obj in return_todos:
+            t = Todo()
+            t.Set(obj)
+            todos.append(t)
+
+    return render_template('index.html', todos=todos)
 
 @app.route("/create", methods = ['GET', 'POST'])
 def createtodo():
@@ -43,6 +60,11 @@ def createtodo():
             print form.task.data
             print form.due_date.data
         return redirect(url_for('index'))
+
+@app.route("/completed/<id>", methods = ['GET'])
+def completed(id):
+
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run()
